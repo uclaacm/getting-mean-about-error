d362263a00f00e18fbec8653a276de9fa1c387da
Michelle Zhuang <michelle.zhuang@g.ucla.edu>
2020-02-24 00:51:05 -0800

    Second Draft

--- js/main.js
+++ js/main.js
@@ -16,22 +16,22 @@
 //             i + Math.random()*0.2,
 //             11*i/8 + 0.5 * Math.random()
 //         ]
-//     )  
+//     )
 // }
 
 let genErrorPoints = [];
 let xCoords = [1,2,3,4,5,6];
 let yCoords = [2,3,6,4,8,9];
-let yIdealOne = [];
-let yIdealTwo = [];
+let idealPointsOne = [];
+let idealPointsTwo = [];
 
 for (let i = 0; i < xCoords.length; i++) {
     genErrorPoints.push([xCoords[i], yCoords[i]]);
-    yIdealOne.push(xCoords[i] + 2.0);
-    yIdealTwo.push(xCoords[i] * 1.5);
+    idealPointsOne.push([xCoords[i], xCoords[i] + 2.0]);
+    idealPointsTwo.push([xCoords[i], xCoords[i] * 1.5]);
 }
-console.log(genErrorPoints);
 
+/*
 let errorGraphOne = {
     target: '#error-graph-one',
     xAxis: {
@@ -51,75 +51,119 @@ let errorGraphOne = {
         }
     ]
 }
+*/
 
-let errorGraphTwo = {
-    target: '#error-graph-two',
+let errorGraphOne = {
+    target: '#error-graph-one',
+    width: 500,
+    disableZoom: true,
     xAxis: {
         label: 'x - axis',
         domain: [0, 7]
     },
     yAxis: {
         label: 'y - axis',
-        domain: [0, 14]
+        domain: [0, 10]
     },
     data: [
+        {
+            fn: 'x + 2', color: 'red',
+            attr: { "stroke-width": 3 }
+        },
+        {
+            fn: '3x/2', color: 'green',
+            attr: { "stroke-width": 3 }
+        },
         {
             points: genErrorPoints,
             fnType: 'points',
             graphType: 'scatter',
-            color: 'blue'
+            color: 'blue',
+            attr: { "stroke-width": 5 }
+        }
+    ]
+}
+
+let errorGraphTwo = {
+    target: '#error-graph-two',
+    width: 500,
+    disableZoom: true,
+    xAxis: {
+        label: 'x - axis',
+        domain: [0, 7]
+    },
+    yAxis: {
+        label: 'y - axis',
+        domain: [0, 10]
+    },
+    annotations:[
+      {x: 1},
+      {x: 2},
+      {x: 3},
+      {x: 4},
+      {x: 5},
+      {x: 6},
+    ],
+    data: [
+        {
+            fn: 'x + 2', color: 'red',
+            attr: { "stroke-width": 3 }
         },
         {
-            fn: 'x + 2', color: 'red'
+          points: idealPointsOne,
+          fnType: 'points',
+          graphType: 'scatter',
+          color: 'black',
+          attr: { "stroke-width": 3 }
         },
         {
-            fn: '3x/2', color: 'green'
+            points: genErrorPoints,
+            fnType: 'points',
+            graphType: 'scatter',
+            color: 'blue',
+            attr: { "stroke-width": 5 }
         }
     ]
 }
 
 let errorGraphThree = {
     target: '#error-graph-three',
+    width: 500,
+    disableZoom: true,
     xAxis: {
         label: 'x - axis',
         domain: [0, 7]
     },
     yAxis: {
         label: 'y - axis',
-        domain: [0, 14]
+        domain: [0, 10]
     },
+    annotations:[
+      {x: 1},
+      {x: 2},
+      {x: 3},
+      {x: 4},
+      {x: 5},
+      {x: 6},
+    ],
     data: [
+        {
+          points: idealPointsTwo,
+          fnType: 'points',
+          graphType: 'scatter',
+          color: 'black',
+          attr: { "stroke-width": 3 }
+        },
         {
             points: genErrorPoints,
             fnType: 'points',
             graphType: 'scatter',
-            color: 'blue'
+            color: 'blue',
+            attr: { "stroke-width": 5 }
         },
         {
-            fn: 'x + 2', color: 'red'
-        }
-    ]
-}
-
-let errorGraphFour = {
-    target: '#error-graph-four',
-    xAxis: {
-        label: 'x - axis',
-        domain: [0, 7]
-    },
-    yAxis: {
-        label: 'y - axis',
-        domain: [0, 14]
-    },
-    data: [
-        {
-            points: genErrorPoints,
-            fnType: 'points',
-            graphType: 'scatter',
-            color: 'blue'
-        },
-        {
-            fn: '3x/2', color: 'green'
+            fn: '3x/2', color: 'green',
+            attr: { "stroke-width": 3 }
         }
     ]
 }
@@ -127,7 +171,7 @@ let errorGraphFour = {
 functionPlot(errorGraphOne);
 functionPlot(errorGraphTwo);
 functionPlot(errorGraphThree);
-functionPlot(errorGraphFour);
+//functionPlot(errorGraphFour);
 
 // // Refresh the data on the graph.
 
@@ -148,7 +192,7 @@ functionPlot(errorGraphFour);
 //             i + Math.random()*0.2,
 //             11*i/8 + 0.5 * Math.random()
 //         ]
-//     )  
+//     )
 //   }
 //   errorGraphOne.data.points = genErrorPoints;
 //   functionPlot(errorGraphOne);
@@ -157,16 +201,78 @@ functionPlot(errorGraphFour);
 
 // document.getElementById("refresh-data").addEventListener("click", refreshPoints);
 
-// Calculate mean-squared error.
+// Calculate mean-squared error; pass in 2D arrays for parameters.
+// MSE = 1.5 for y = x+2; MSE = 1.125 for y = 3x/2
 
-function meanSquaredError() {
+function meanSquaredError( pointsOne, pointsTwo ) {
   let sum = 0;
-  for (let i = 0; i < genErrorPoints.length; i++) {
+  for (let i = 0; i < pointsOne.length; i++) {
     // add squared distances.
-    let y1 = genErrorPoints[i][1];
-    //let y2 = function(genErrorPoints[i][0]);
-    sum += (y1 - y2)**2.;
+    let y1 = pointsOne[i][1];
+    let y2 = pointsTwo[i][1];
+    sum += (y2 - y1)**2.;
   }
-  return sum / getErrorPoints.length;
+  return sum / pointsOne.length;
 }
 
+
+// Generate MSE table for demo
+let table=document.getElementById("msetable");
+
+let col = document.createElement("tr");
+
+let cellX = document.createElement("th");
+let cellOurY = document.createElement("th");
+let cellPrdY = document.createElement("th");
+
+let dataX = document.createTextNode("x coordinate");
+let dataOurY = document.createTextNode("expected y coordinate");
+let dataPrdY = document.createTextNode("original y coordinate");
+
+cellX.appendChild(dataX);
+cellOurY.appendChild(dataOurY);
+cellPrdY.appendChild(dataPrdY);
+col.appendChild(cellX);
+col.appendChild(cellOurY);
+col.appendChild(cellPrdY);
+table.appendChild(col);
+
+for (let i = 0; i < genErrorPoints.length; i++) {
+  col = document.createElement("tr");
+
+  cellX = document.createElement("td");
+  cellOurY = document.createElement("td");
+  cellPrdY = document.createElement("td");
+
+  dataX = document.createTextNode(xCoords[i]);
+  dataOurY = document.createTextNode(idealPointsOne[i][1]);
+  dataPrdY = document.createTextNode(yCoords[i]);
+
+  cellX.appendChild(dataX);
+  cellOurY.appendChild(dataOurY);
+  cellPrdY.appendChild(dataPrdY);
+  col.appendChild(cellX);
+  col.appendChild(cellOurY);
+  col.appendChild(cellPrdY);
+  table.appendChild(col);
+}
+
+let mseInput = document.getElementById("mse-input").value;
+console.log(mseInput);
+document.getElementById("submit").addEventListener("click", checkMSE);
+
+function checkMSE() {
+  // MSE for demo is 1.125
+  document.getElementById("check-answer").style.display = "block"
+  let msgTxt;
+  let mseInput = document.getElementById("mse-input").value;
+
+  if (mseInput === String(meanSquaredError(genErrorPoints, idealPointsTwo))) {
+    msgTxt = "Correct!";
+  }
+  else {
+    msgTxt = "Incorrect!";
+  }
+  document.getElementById("check-answer").textContent = msgTxt;
+  setTimeout(function() {document.getElementById("check-answer").style.display = "none"}, 1000);
+}
